42f0b3dbf550714f12e259490f6034c683d4add6
VisionShots <145379710+VisionShots@users.noreply.github.com>
2023-10-08 19:16:24 +0530

    Update main.js

--- js/main.js
+++ js/main.js
@@ -137,3 +137,17 @@ function setImageGallery(path, imgNames) {
 
 }
 
+function enableSubmit(){
+    let inputs = document.getElementsByClassName('required'); // Enter your class name for a required field, this should also be reflected within your form fields.
+    let btn = document.getElementById('form-submit');
+    let isValid = true;
+    for (var i = 0; i < inputs.length; i++){
+        let changedInput = inputs[i];
+        if (changedInput.value.trim() === "" || changedInput.value === null){
+        isValid = false;
+        break;
+        }
+    }
+    btn.disabled = !isValid;
+}
+
